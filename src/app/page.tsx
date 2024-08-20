4751fc513ce82cf207c6ee52b61382ba83ee7b9d
Ben Webb <ben.webb340@gmail.com>
2024-08-20 16:08:33 +0900

    fix fonts

--- src/app/page.tsx
+++ src/app/page.tsx
@@ -35,21 +35,27 @@ const Home: React.FC = () => {
     const matrix = () => {
       if (!ctx) return;
 
-      ctx.fillStyle = 'rgba(0, 0, 0, 0.1)';  // Slightly increase the transparency for a dimmer effect
+      ctx.fillStyle = 'rgba(0, 0, 0, 0.1)';
       ctx.fillRect(0, 0, width, height);
 
-      ctx.fillStyle = 'rgba(0, 255, 204, 0.4)';  // Dim the brightness slightly
+      ctx.fillStyle = 'rgba(0, 255, 204, 0.4)';
       ctx.font = '18px monospace';
 
       yPositions.forEach((y, index) => {
         const text = String.fromCharCode(Math.random() * 128);
         const x = index * 20;
         ctx.fillText(text, x, y);
-        yPositions[index] = y > height ? 0 : y + 15;
+
+        // Randomize the speed for each column
+        if (y > height && Math.random() > 0.975) {
+          yPositions[index] = 0;
+        } else {
+          yPositions[index] = y + Math.random() * 20 + 10;
+        }
       });
     };
 
-    const interval = setInterval(matrix, 60);
+    const interval = setInterval(matrix, 100);
 
     return () => clearInterval(interval);
   }, []);
